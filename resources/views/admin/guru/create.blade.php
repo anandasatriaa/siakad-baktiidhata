c30168e54bcb90a69d2137d1a8ec6cdae9270976
Ananda Satria <anandasatriaf6@gmail.com>
2026-06-07 13:41:22 +0700

    feat: add create and edit views for teacher data management

--- resources/views/admin/guru/create.blade.php
+++ resources/views/admin/guru/create.blade.php
@@ -33,8 +33,8 @@
                                 @error('nip') <div class="invalid-feedback">{{ $message }}</div> @enderror
                             </div>
                             <div class="col-md-6 form-group mb-3">
-                                <label for="nik">NIK</label>
-                                <input type="text" id="nik" class="form-control @error('nik') is-invalid @enderror" name="nik" value="{{ old('nik') }}" placeholder="Masukkan NIK">
+                                <label for="nik">NIK <span class="text-danger">*</span></label>
+                                <input type="text" id="nik" class="form-control @error('nik') is-invalid @enderror" name="nik" value="{{ old('nik') }}" required placeholder="Masukkan NIK">
                                 @error('nik') <div class="invalid-feedback">{{ $message }}</div> @enderror
                             </div>
                         </div>
